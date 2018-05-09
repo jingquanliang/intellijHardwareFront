3e715043f9648b9a06c2e72c4060cad99f2b75ae
jingquanliang <540692237@qq.com>
2018-05-09 17:11:26 +0800

    增加了社交页面

---
+++ pages/discover/discover.js
@@ -0,0 +1,23 @@
+//discover.js
+const util = require('../../utils/util.js')
+
+var initFilterTitle = '过滤'
+var initFilterContent =""
+Page({
+  data: {
+    filterText: initFilterTitle,
+    filterContent: initFilterContent
+  },
+  onLoad: function () {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map(log => {
+        return util.formatTime(new Date(log))
+      })
+    })
+  },
+  toSocial: function () {
+    wx.navigateTo({
+      url: 'nearPerson/nearPerson',
+    })
+  },
+})
--- pages/discover/nearPerson/nearPerson.js
+++ pages/discover/nearPerson/nearPerson.js
@@ -1,5 +1,5 @@
-//logs.js
-const util = require('../../utils/util.js')
+//discover.js
+const util = require('../../../utils/util.js')
 
 var initFilterTitle = '过滤'
 var initFilterContent =""
